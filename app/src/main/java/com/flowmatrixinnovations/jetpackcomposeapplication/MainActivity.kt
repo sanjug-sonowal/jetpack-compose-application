06bcb96f4b6fef3cdf7b9060b637f1db1eb66b89
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-19 18:41:37 +0530

    add row and colomn practice composable and placed in the preview

--- app/src/main/java/com/flowmatrixinnovations/jetpackcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/flowmatrixinnovations/jetpackcomposeapplication/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.flowmatrixinnovations.jetpackcomposeapplication.ui.theme.JetpackComposeApplicationTheme
+import com.flowmatrixinnovations.jetpackcomposeapplication.ui.theme.Pink40
+import com.flowmatrixinnovations.jetpackcomposeapplication.ui.theme.Pink80
+import com.flowmatrixinnovations.jetpackcomposeapplication.ui.theme.Purple40
 import com.flowmatrixinnovations.jetpackcomposeapplication.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
@@ -63,13 +71,58 @@ fun SanjugCustomComposable(name:String,rollNo: Int){
     }
 }
 
+@Composable
+fun CustomColumnPractice(){
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Surface(
+            color = Purple40,
+            modifier = Modifier.padding(8.dp)
+                .width(200.dp)
+                .height(40.dp)
+        ) { }
+
+        Surface(
+            color = Pink40,
+            modifier = Modifier.padding(8.dp)
+                .width(200.dp)
+                .height(40.dp)
+        ) { }
+    }
+}
+
+@Composable
+fun CustomRowPractice(){
+    Row(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Surface(
+            color = Purple40,
+            modifier = Modifier.padding(8.dp)
+                .width(200.dp)
+                .height(40.dp)
+        ) {
+
+        }
+        Surface(
+            color = Pink80,
+            modifier = Modifier.padding(8.dp)
+                .width(200.dp)
+                .height(40.dp)
+        ) { }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeApplicationTheme {
-        Column() {
-            Greeting("Android")
-            SanjugCustomComposable("Sanjug",21)
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            CustomRowPractice()
+            CustomColumnPractice()
         }
     }
 }
