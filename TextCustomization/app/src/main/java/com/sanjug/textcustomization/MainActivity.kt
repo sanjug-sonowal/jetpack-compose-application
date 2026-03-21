57b06bab48f4140ef547455427cc50bf0477cf88
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-21 15:16:30 +0530

    Custom Text

---
+++ TextCustomization/app/src/main/java/com/sanjug/textcustomization/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.sanjug.textcustomization
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sanjug.textcustomization.ui.theme.TextCustomizationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TextCustomizationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomText(
+    text: String,
+    modifier: Modifier = Modifier,
+){
+    Column() {
+        Text(
+            text = text,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TextCustomizationTheme {
+        Column() {
+            Greeting("Android")
+            CustomText("sanjug",Modifier)
+        }
+    }
+}
